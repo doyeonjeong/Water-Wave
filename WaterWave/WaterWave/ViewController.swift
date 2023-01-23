dfead1a5e4984e9aca323662275c38db0040188d
Debby <debby_@kakao.com>
2023-01-23 15:20:46 +0900

    ✨ 기본 틀 생성

--- WaterWave/WaterWave/ViewController.swift
+++ WaterWave/WaterWave/ViewController.swift
@@ -7,10 +7,25 @@
 
 import UIKit
 
+let screenWidth = UIScreen.main.bounds.size.width
+
 class ViewController: UIViewController {
+    
+    let waterWaveView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        waterWaveView.clipsToBounds = true
+        waterWaveView.backgroundColor = .cyan
+        view.addSubview(waterWaveView)
+        waterWaveView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            waterWaveView.widthAnchor.constraint(equalToConstant: screenWidth * 0.5),
+            waterWaveView.heightAnchor.constraint(equalToConstant: screenWidth * 0.5),
+            waterWaveView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            waterWaveView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 }
