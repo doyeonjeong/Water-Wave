2c099005da9a5872521dd0fd00ff908924010d2c
Debby <debby_@kakao.com>
2023-01-23 15:15:40 +0900

    🎉 프로젝트 생성

---
+++ WaterWave/WaterWave/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WaterWave
+//
+//  Created by Doyeon on 2023/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
